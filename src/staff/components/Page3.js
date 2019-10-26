3431832c4bfa599efc0d3207aa4a27921a605650
raufnawazrtarar <33519174+raufnawazrtarar@users.noreply.github.com>
2019-10-26 22:58:06 +0100

    Added Pie chart for the ratings

--- src/staff/components/Page3.js
+++ src/staff/components/Page3.js
@@ -2,6 +2,33 @@ import React from 'react';
 import { Header, Container, Segment } from 'semantic-ui-react';
 import '/./feedback.css';
 import logo from '/./gsc_logo.svg';
+import {Pie} from 'react-chartjs-2'
+
+
+const data = {
+    legend:[
+        
+
+    ],
+	labels: [
+		'Happy',
+		'Neutrel',
+		'Unhappy'
+	],
+	datasets: [{
+		data: [51, 20, 10,10,9],
+		backgroundColor: [
+		'#00ff00',
+		'#ffff00',
+		'#ff0000'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
 
 const Page3 = () => (
     
@@ -11,6 +38,10 @@ const Page3 = () => (
             <img class = 'logo' src={logo}/>
         </div>
         <p class = 'Title'>Feedback Center</p>
+        <div>
+          <h2>Rating</h2>
+          <Pie data={data} />
+        </div>
     </Container>
 );
 
