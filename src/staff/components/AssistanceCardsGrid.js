742d5fb9f87e4a9f36c12fb6fc7e18207af239ac
Gabrijel Boduljak <gboduljak@outlook.com>
2019-10-27 07:44:01 +0000

    implemented direct communication

--- src/staff/components/AssistanceCardsGrid.js
+++ src/staff/components/AssistanceCardsGrid.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Card, Dropdown, Form } from 'semantic-ui-react';
 import AssistanceRequestCard from './AssistanceRequestCard';
 import { getAssistanceRequests } from '../ApiService';
-import ReactNotification from 'react-notifications-component';
-import { store } from 'react-notifications-component';
-import 'react-notifications-component/dist/theme.css';
 import MiniFeedback from './MiniFeedback';
 
 const assistanceOptions = [
@@ -18,28 +15,10 @@ export default class AssistanceCardsGrid extends Component {
         super(props);
         this.state = {};
     }
+    componentWillUnmount() {}
 
     componentDidMount() {
         window.socket.on('staff:newAssistanceRequest', payload => {
-            store.addNotification({
-                title: 'New Assistance request received!',
-                message: `${payload.id}`,
-                type: 'danger',
-                insert: 'top',
-                container: 'top-right',
-                animationIn: ['animated', 'fadeIn'],
-                animationOut: ['animated', 'fadeOut'],
-                dismiss: {
-                    duration: 20000,
-                    onScreen: true,
-                },
-                onRemoval: (id, removedBy) => {
-                    if (removedBy == 'click') {
-                        console.log('handle open details for this notification');
-                    }
-                },
-            });
-
             this.setState({ easyGridReload });
             easyGridReload();
         });
@@ -80,7 +59,6 @@ export default class AssistanceCardsGrid extends Component {
 
         return (
             <div>
-                <ReactNotification />
                 <Form>
                     <Dropdown
                         button
--- src/staff/components/Realtime.js
+++ src/staff/components/Realtime.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import socketIOClient from 'socket.io-client';
 import apiUrl from '../apiUrl';
+import ReactNotification from 'react-notifications-component';
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
 
 export default class Realtime extends Component {
     constructor() {
@@ -17,10 +20,39 @@ export default class Realtime extends Component {
             this.setState({ response: payload });
         });
 
+        socket.on('staff:newAssistanceRequest', event => {
+            const assistanceRequestType =
+                event.assistanceRequest &&
+                event.assistanceRequest.payload &&
+                event.assistanceRequest.payload.assistanceRequestDetails &&
+                event.assistanceRequest.payload.assistanceRequestDetails.aidType
+                    ? event.assistanceRequest.payload.assistanceRequestDetails.aidType.toLowerCase()
+                    : '';
+
+            store.addNotification({
+                title: 'New Assistance request received!',
+                message: `It is of ${assistanceRequestType} request type.`,
+                type: 'danger',
+                insert: 'top',
+                container: 'top-right',
+                animationIn: ['animated', 'fadeIn'],
+                animationOut: ['animated', 'fadeOut'],
+                dismiss: {
+                    duration: 20000,
+                    onScreen: true,
+                },
+                onRemoval: (id, removedBy) => {
+                    if (removedBy == 'click') {
+                        console.log('handle open details for this notification');
+                    }
+                },
+            });
+        });
+
         window.socket = socket;
     }
 
     render() {
-        return <div></div>;
+        return <ReactNotification />;
     }
 }
